06e49632177056a7d4dc2a6aa465a449e186b622
Harry Fox <henry.george.fox@gmail.com>
2015-03-19 18:03:51 +0000

    Updated with greater compatability

--- scroll.js
+++ scroll.js
@@ -1,17 +1,25 @@
-var screen1 = document.getElementById("screen1");
-var title = document.getElementById("title");
-var lightbulb = document.getElementById("lightbulb");
-
-
-// Applies a 25% movement in the OPPOSITE direction to the scroll so as to simulate slow movement up the page.
-var slow = 4;
-// inverted it will speed up the movement up of an element by 4
-
-
+var screen1 = document.getElementById("screen1"),
+    title = document.getElementById("title"),
+    lightbulb = document.getElementById("lightbulb"),
+    slow = 4,
+    fast = 0.5,
+    fadeSpeed = 300,
+    y,
+    // Applies a 25% movement in the OPPOSITE direction to the scroll so as to simulate slow movement up the page.
+        // inverted it will speed up the movement up of an element by 4
+    supportPageOffset = window.pageXOffset !== undefined,
+        // true or false, browser does not have property .pageXOffset
+        // if false (and property is undefined then the browser is probably ie8)
+    isCSS1Compat = ((document.compatMode || "") === "CSS1Compat");
+        // true or false, the document is rendered in "Standards mode"
+        // if false (and is "BackCompat"), document is rendered in Quirks mode and is probably old.
+        
 window.onscroll = function() {
-    var y = window.scrollY;
+    y = supportPageOffset ? window.pageYOffset : isCSS1Compat ? document.documentElement.scrollTop : document.body.scrollTop;
+       // defines y as pageYOffset if modern browser
     screen1.style.backgroundPosition = "0px " + (y / slow) + "px";
-    title.style.opacity = "1" - (y/300);
-    title.style.transform = "translate(0px," + (y/-slow) + "px)";
-    lightbulb.style.transform = "translate(0px," + (y/-0.5) + "px)";
+        // Move elements background position not element itself
+    title.style.opacity = "1" - (y / fadeSpeed);
+    title.style.transform = "translate(0px," + (y / -slow) + "px)";
+    lightbulb.style.transform = "translate(0px," + (y / -fast) + "px)";
 }
